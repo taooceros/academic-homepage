d1731442762101941dc37a5ba2e9b3778c1ae907
Hongtao Zhang <hongtao_zhang@outlook.com>
2025-06-24 15:05:18 -0500

    try fix config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ahxt.github.io/academic-homepage-typst',
-  base: '/academic-homepage-typst/',
+  site: 'https://academic-homepage.vercel.app/',
+  base: '/',  // Comment out for local development
   
   integrations: [
     sitemap({
